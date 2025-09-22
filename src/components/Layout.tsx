696172f6328eb21bcf20132d783d724b3baac516
rohdge-picode <130414349+rohdge-picode@users.noreply.github.com>
2025-09-22 22:12:46 +0530

    removed admin from menubar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -22,7 +22,7 @@ const navigation = [
   { name: "Registers", href: "/registers", icon: Database },
   { name: "Datasets", href: "/datasets", icon: Layers },
   { name: "Dissemination", href: "/dissemination", icon: Share2 },
-  { name: "Admin", href: "/admin", icon: Shield },
+  // { name: "Admin", href: "/admin", icon: Shield },
 ];
 
 export const Layout = ({ children }: LayoutProps) => {
